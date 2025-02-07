cbf3655fa125789566547b6b484c02d2805f37b7
DAYNLIGHT <danielstodulski000@gmail.com>
2025-02-07 01:38:55 +0100

    refactor window

--- PixForge/Core/Window/Window.cpp
+++ PixForge/Core/Window/Window.cpp
@@ -2,37 +2,25 @@
 
 PF::Window::Window(const char* title) {
   Folder settings = Folder("settings/");
-  if(!settings.exist()) settings.createFolder();
+  loadWindowSettings();
 
   if(SDL_Init(SDL_INIT_EVERYTHING)) throw std::runtime_error("SDL Init Error");
+  
   createWindow(title);
   createRenderer();
 };
 
 PF::Window::~Window(){
-  SDL_Rect window_location = getWindowSizesAndPosition();
-  if(window_settings.isEmpty()) window_settings.createFile();
-  window_settings.clear();
-  window_settings.push(std::to_string(window_location.x));
-  window_settings.push(std::to_string(window_location.y));
-  window_settings.push(std::to_string(window_location.w));
-  window_settings.push(std::to_string(window_location.h));
-  window_settings.push(std::to_string(SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN_DESKTOP != 0));
-  window_settings.save();
-
+  saveWindowSettings();
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
 };
 
 inline void PF::Window::createWindow(const char *title){
-  if(window_settings.isEmpty()) {
+  if(window_settings.isEmpty())
     window = SDL_CreateWindow(title, WINDOW_POSITION, WINDOW_SIZES, WINDOW_FLAGS);
-    if(!window) throw std::runtime_error("Can't create window");
-  }
   else{
-    window_settings.read();
-    
     window = SDL_CreateWindow(title, std::stoi(window_settings[0]), std::stoi(window_settings[1]), 
       std::stoi(window_settings[2]), std::stoi(window_settings[3]), WINDOW_FLAGS);
     if(!window) throw std::runtime_error("Can't create window");
@@ -46,6 +34,22 @@ inline void PF::Window::createWindow(const char *title){
 inline void PF::Window::createRenderer(){
   renderer = SDL_CreateRenderer(window, 0, 0);
   if(!renderer) throw std::runtime_error("Can't create renderer");
+};
+
+inline void PF::Window::saveWindowSettings() {
+  SDL_Rect window_location = getWindowSizesAndPosition();
+  if(window_settings.isEmpty()) window_settings.createFile();
+  window_settings.clear();
+  window_settings.push(std::to_string(window_location.x));
+  window_settings.push(std::to_string(window_location.y));
+  window_settings.push(std::to_string(window_location.w));
+  window_settings.push(std::to_string(window_location.h));
+  window_settings.push(std::to_string(SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN_DESKTOP != 0));
+  window_settings.save();
+};
+
+inline void PF::Window::loadWindowSettings() {
+  window_settings.read();
 };
 
 SDL_Rect PF::Window::getWindowSizesAndPosition(){
@@ -68,7 +72,3 @@ inline void PF::Window::changeFullScreenDesktop(){
   else SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
   SDL_Delay(500);
 };
-
-bool PF::Window::isRunning() { return running; }
-SDL_Renderer *PF::Window::getRenderer() { return renderer; }
-SDL_Window *PF::Window::getWindow() { return window; }
