c5888b13bb0a994aeca3181cfc6cbf1e056bd499
DAYNLIGHT <danielstodulski000@gmail.com>
2025-02-09 07:59:59 +0100

    Check for leaks !!! File Explorer is Broken !!! + 82 another leaks Object saves breaks

--- PixForge/Forge/Main.cpp
+++ PixForge/Forge/Main.cpp
@@ -1,7 +1,26 @@
 #include "Forge/Forge.h"
 
+#include "../Core/STL/File/File.h"
+
+size_t allocationCount = 0;
+void* operator new(size_t size){
+  allocationCount++;
+  return malloc(size);
+}
+
+void operator delete(void* memory){
+  allocationCount--;
+  free(memory);
+}
+
 int WinMain(){
-  PF::Forge::Forge forge;
-  forge.run();
+  {
+    PF::Forge::Forge forge;
+    forge.run();
+  }
+
+  PF::STL::File file("Allocations.txt");
+  file.push("Allocations: " + std::to_string(allocationCount));
+  file.save();
   return 0;
 };
