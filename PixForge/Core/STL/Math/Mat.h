4c7aee994e4395b51f67dc05a4e0b75d3954cdf7
DAYNLIGHT <danielstodulski000@gmail.com>
2025-02-12 04:07:22 +0100

    Mat returns type

--- PixForge/Core/STL/Math/Mat.h
+++ PixForge/Core/STL/Math/Mat.h
@@ -6,23 +6,23 @@ class Mat{
 public:
   T data[S][R];
 public:
-  Mat(T value){ for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] = value; };
-  Mat(std::initializer_list<T> value) { auto it = value.begin(); for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++, ++it) data[i][j] = *it; };
+  Mat(const T &value){ for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] = value; };
+  Mat(const std::initializer_list<T> &value) { auto it = value.begin(); for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++, ++it) data[i][j] = *it; };
   Mat(const Mat<T, S, R> &second){ for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] = second.data[i][j]; };
 public:
-  void operator=(Mat<T, S, R> &second) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] = second[i][j]; };
-  void operator+(Mat<T, S, R> &second) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] += second[i][j]; };
-  void operator-(Mat<T, S, R> &second) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] -= second[i][j]; };
-  void operator*(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] *= value; };
-  void operator/(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] /= value; };
-  void operator%(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] %= value; };
-  void operator+=(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] += value; };
-  void operator-=(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] -= value; };
-  void operator*=(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] *= value; };
-  void operator/=(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] /= value; };
-  void operator%=(T &value) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] %= value; };
+  void operator=(const Mat<T, S, R> &second) { for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) data[i][j] = second[i][j]; };
+  Mat<T, S, R> operator+(const Mat<T, S, R> &second) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] += second[i][j]; return temp; };
+  Mat<T, S, R> operator-(const Mat<T, S, R> &second) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] -= second[i][j]; return temp; };
+  Mat<T, S, R> operator*(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] *= value; return temp; };
+  Mat<T, S, R> operator/(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] /= value; return temp; };
+  Mat<T, S, R> operator%(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] %= value; return temp; };
+  Mat<T, S, R> operator+=(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] += value; return temp; };
+  Mat<T, S, R> operator-=(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] -= value; return temp; };
+  Mat<T, S, R> operator*=(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] *= value; return temp; };
+  Mat<T, S, R> operator/=(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] /= value; return temp; };
+  Mat<T, S, R> operator%=(const T &value) { Mat<T, S, R> temp = *this; for(unsigned int i = 0; i < S; i++) for(unsigned int j = 0; j < R; j++) temp[i][j] %= value; return temp; };
 public:
-  void operator*(Mat<T, S, R> &second);
+  Mat<T, R, S> operator*(const Mat<T, S, R> &second) const;
 public:
   T& operator()(const unsigned int &row, const unsigned int &col) { return data[row][col]; };
   const T& operator()(const unsigned int &row, const unsigned int &col) const { return data[row][col]; };
