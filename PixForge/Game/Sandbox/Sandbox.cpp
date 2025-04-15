b6949a76deeb457d23b5b4062a47410d44fd416b
Daynlight <danielstodulski000@gmail.com>
2025-04-15 05:28:13 -0400

    clean

--- PixForge/Game/Sandbox/Sandbox.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sandbox.h"
-
-void PF::Sandbox::run(){
-    for(size_t i = 0; i < Core::Renderer::Objects::Manager::size(); i++) Core::Renderer::Objects::Manager::at(i).render(window);
-}
-
-void PF::Sandbox::event(SDL_Event* event){
-    
-};
