71ce2bd1ac7d4d0b647fd99c603812fc1890159a
DAYNLIGHT (✿◡‿◡) <danielstodulski000@gmail.com>
2025-06-17 07:56:35 +0200

    restore STL

---
+++ PixForge/STL/Math/Vec.hpp
@@ -0,0 +1,27 @@
+#include "Vec.h"
+
+template<typename T, unsigned int S>
+template<typename U>
+PF::STL::Vec<T, S> PF::STL::Vec<T, S>::operator*(const Mat<U, S, S>& matrix) const {
+    PF::STL::Vec<T, S> result;
+    for (unsigned int i = 0; i < S; i++) {
+        U temp = 0;
+        for (unsigned int j = 0; j < S; j++) {
+            temp += matrix(i, j) * data[j];
+        };
+        result[i] = static_cast<T>(temp);
+    };
+    return result;
+};
+
+template <typename T, unsigned int S>
+template <typename U>
+inline void PF::STL::Vec<T, S>::operator*=(const Mat<U, S, S> &matrix) {
+    for (unsigned int i = 0; i < S; i++) {
+        U temp = 0;
+        for (unsigned int j = 0; j < S; j++) {
+            temp += matrix(i, j) * data[j];
+        };
+        data[i] = static_cast<T>(temp);
+    };
+};
