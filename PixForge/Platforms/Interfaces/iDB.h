c12277159d7323dc535b287f0e4868db4e9ac4cd
DAYNLIGHT (✿◡‿◡) <vampdaynlight@gmail.com>
2025-08-18 22:59:45 +0200

    sql insert works

--- PixForge/Platforms/Interfaces/iDB.h
+++ PixForge/Platforms/Interfaces/iDB.h
@@ -1,12 +1,16 @@
 #pragma once
 #include <string>
+#include <vector>
+#include "Forge/Utilities/Log.h"
 
 namespace PF::PLATFORM{
 class iDB{
 private:
   std::string path = "";
 public:
+  virtual ~iDB() = default;
   virtual void setPath(const std::string& path) = 0;
-
+  virtual std::vector<std::vector<std::string>> select(const std::string& query) = 0;
+  virtual void insert(const std::string& query) = 0;
 };
 }; // namespace PF::PLATFORM
--- PixForge/Platforms/Platform.h
+++ PixForge/Platforms/Platform.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Interfaces/iRenderer.h"
 #include "Interfaces/iEditorGui.h"
+#include "Interfaces/iDB.h"
 
 #include "OpenGL/WIN/Renderer/Renderer.h"
 #include "OpenGL/WIN/EditorGui/EditorGui.h"
+#include "Data/DB.h"
