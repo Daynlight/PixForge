13ea77b7773fbdd5004d57ddeed581ae5c67c823
DAYNLIGHT (✿◡‿◡) <vampdaynlight@gmail.com>
2025-08-29 10:13:34 +0200

    Utilities Clean up

--- PixForge/Utilities/Math/Vec.h
+++ PixForge/Utilities/Math/Vec.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "Mat.h"
 
 namespace PF::UTILITIES{
 template<typename T, unsigned int S>
@@ -29,14 +28,7 @@ public:
   Vec<T, S> operator%(const T &value) const { Vec<T, S> temp(*this); for(unsigned int i = 0; i < S; i++) temp[i] %= value; return temp; };
   void operator%=(const T &value) { for(unsigned int i = 0; i < S; i++) data[i] %= value; };
 public:
-  template<typename U>
-  Vec<T, S> operator*(const Mat<U, S, S> &matrix) const;
-  template<typename U>
-  void operator*=(const Mat<U, S, S> &matrix);
-public:
   T& operator[](const unsigned int &index) { return data[index]; };
   const T& operator[](const unsigned int &index) const { return data[index]; };
 }; // class Vec
-}; // namespace PF::STL
-
-#include "Vec.hpp"
+}; // namespace PF::UTILITIES
